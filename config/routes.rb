47e6713d8551898581fe4c2a76ae2fc6a6b71011
Bobby Caya <bob.caya1@gmail.com>
2018-03-23 11:51:00 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  namespace :api do 
+    resources :todos 
+  end 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*other', to: 'static#index' 
+end
